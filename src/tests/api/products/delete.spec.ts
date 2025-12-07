5184dbcf5b7804e8a9c2eb06b031c2275e22e0fa
Valeria Shut <valeria.shut@gmail.com>
2025-12-07 19:44:21 +0300

    add test Delete product without TOKEN

--- src/tests/api/products/delete.spec.ts
+++ src/tests/api/products/delete.spec.ts
@@ -6,7 +6,7 @@ test.describe("[API] [Sales Portal] [Products]", () => {
 	test(
 		"Delete Product",
 		{
-			tag: [TAGS.PRODUCTS],
+			tag: [TAGS.PRODUCTS, TAGS.SMOKE],
 		},
 		async ({ loginApiService, productsApiService, productsApi }) => {
 			const token = await loginApiService.loginAsAdmin();
@@ -22,7 +22,7 @@ test.describe("[API] [Sales Portal] [Products]", () => {
 	test(
 		"Delete a deleted Product",
 		{
-			tag: [TAGS.PRODUCTS],
+			tag: [TAGS.PRODUCTS, TAGS.REGRESSION],
 		},
 		async ({ loginApiService, productsApiService, productsApi }) => {
 			const token = await loginApiService.loginAsAdmin();
@@ -35,6 +35,22 @@ test.describe("[API] [Sales Portal] [Products]", () => {
 
 			const response = await productsApi.delete(id, token);
 			expect(response.status).toBe(STATUS_CODES.NOT_FOUND);
+		},
+	);
+
+	test(
+		"Delete Product without TOKEN",
+		{
+			tag: [TAGS.PRODUCTS, TAGS.REGRESSION],
+		},
+		async ({ loginApiService, productsApiService, productsApi }) => {
+			const token = await loginApiService.loginAsAdmin();
+
+			const createdProduct = await productsApiService.create(token);
+			const id = createdProduct._id;
+
+			const responsefirst = await productsApi.delete(id, "");
+			expect(responsefirst.status).toBe(STATUS_CODES.UNAUTHORIZED);
 		},
 	);
 });
