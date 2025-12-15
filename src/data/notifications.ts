56f0432233a713ec4ac91ba5fe5894302ae3166f
maratgefter <47002971+maratgefter@users.noreply.github.com>
2025-12-15 07:26:21 +0300

    Add comments (#35)
    * tests for comment add and delete
    
    * tests for comment add and delete
    
    * fixes

--- src/data/notifications.ts
+++ src/data/notifications.ts
@@ -5,6 +5,7 @@ export enum NOTIFICATIONS {
 	CREATED_FAIL_BAD_REQUEST = "CREATED_FAIL_BAD_REQUEST",
 	CREATED_FAIL_INCORRET_REQUEST_BODY = "Incorrect request body",
 	CUSTOMER_CREATED = "Customer was successfully created",
+	COMMENT_WAS_NOT_FOUND = "Comment was not found",
 }
 
 export const ERROR_MESSAGES = {
--- src/tests/api/orders/deleteCommemtById.spec.ts
+++ src/tests/api/orders/deleteCommemtById.spec.ts
@@ -1,13 +1,60 @@
-import test from "@playwright/test";
+import { ERROR_MESSAGES, NOTIFICATIONS } from "data/notifications";
+import { generateProductData } from "data/products/generateProductData";
+import { STATUS_CODES } from "data/statusCodes";
+import { expect, test } from "fixtures/api.fixture";
+import { randomString } from "utils/randomStringsGeneration";
+import { validateResponse } from "utils/validation/validateResponse.utils";
 
 test.describe("[API] [Sales Portal] [Orders] [Comments] [Delete Comment By Id]", () => {
-	test.skip("Delete comment by valid id", async () => {});
+	let token = "";
+	let idCustomer = "";
+	let idProduct = "";
+	let idOrder = "";
+	let idComment = "";
 
-	test.skip("Delete comment by valid without auth token", async () => {});
+	test.beforeAll(async ({ loginApiService, customersApiService, productsApi, ordersApiService }) => {
+		token = await loginApiService.loginAsAdmin();
+		const customer = await customersApiService.create(token);
+		idCustomer = customer._id;
+		const productData = generateProductData();
+		const createdProduct = await productsApi.create(productData, token);
 
-	test.skip("Delete comment by invalid id", async () => {});
+		idProduct = createdProduct.body.Product._id;
 
-	test.skip("Delete comment by already deleted id", async () => {});
+		const createOrderForCustomer = await ordersApiService.createDraft(token, 2);
+		idOrder = createOrderForCustomer._id;
 
-	test.skip("Try to delete comment by id from another order", async () => {});
+		const comment = await ordersApiService.addComment(token, randomString(75), idOrder);
+		if (comment.body.Order.comments?.[0]?._id) {
+			idComment = comment.body.Order.comments?.[0]?._id;
+		}
+	});
+
+	test.afterAll(async ({ ordersApiService }) => {
+		await ordersApiService.fullDelete(token, [idOrder], [idCustomer], [idProduct]);
+	});
+
+	test("Delete comment by valid id", async ({ ordersApi }) => {
+		const deleteComment = await ordersApi.deleteCommentFromOrder(idOrder, idComment, token);
+		expect(deleteComment.status).toBe(STATUS_CODES.DELETED);
+	});
+
+	test("Delete comment by valid id without auth token", async ({ ordersApi }) => {
+		const deleteComment = await ordersApi.deleteCommentFromOrder(idOrder, idComment, "");
+		validateResponse(deleteComment, {
+			status: STATUS_CODES.UNAUTHORIZED,
+			IsSuccess: false,
+			ErrorMessage: ERROR_MESSAGES.UNAUTHORIZED,
+		});
+	});
+
+	test("Delete comment by invalid id", async ({ ordersApi }) => {
+		const _id = "6894b2471c508c5d5e93e111";
+		const deleteComment = await ordersApi.deleteCommentFromOrder(idOrder, _id, token);
+		validateResponse(deleteComment, {
+			status: STATUS_CODES.BAD_REQUEST,
+			IsSuccess: false,
+			ErrorMessage: NOTIFICATIONS.COMMENT_WAS_NOT_FOUND,
+		});
+	});
 });
