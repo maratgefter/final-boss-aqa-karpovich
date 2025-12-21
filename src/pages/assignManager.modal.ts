94866cc442eef56aaa74940d89c102d179dc4098
Yuriy Shevtsov <yuriy.shevtsov@geozilla.com>
2025-12-21 15:23:03 +0000

    modals

---
+++ src/pages/assignManager.modal.ts
@@ -0,0 +1,38 @@
+import { logStep } from "utils/report/logStep.utils";
+import { ConfirmationModal } from "./confirmation.modal";
+
+export class assignedManagerModal extends ConfirmationModal {
+	readonly uniqueElement = this.page.locator("modal-content");
+
+	readonly title = this.uniqueElement.locator("h5");
+	readonly searchField = this.uniqueElement.locator("#manager-search-input");
+	readonly managerList = this.uniqueElement.locator("#manager-list");
+	readonly managerID = (id: string) => this.managerList.locator(`//li[@data-managerid="${id}"]`);
+	readonly saveButton = this.uniqueElement.locator("btn-primary");
+	readonly closeButton = this.uniqueElement.locator("button.hover-danger");
+
+	@logStep("Click close button")
+	async clickClose() {
+		await this.closeButton.click();
+	}
+
+	@logStep("Click cancel button")
+	async clickCancel() {
+		await this.cancelButton.click();
+	}
+
+	@logStep("Click save button")
+	async clickSave() {
+		await this.saveButton.click();
+	}
+
+	@logStep("Click search field")
+	async clickSearchField() {
+		await this.searchField.click();
+	}
+
+	@logStep("Click on manager by id")
+	async clickManagerById(id: string) {
+		await this.managerID(id).click();
+	}
+}
